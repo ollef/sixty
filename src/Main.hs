c590999aed3ee864ec9356c92a9c839908cb0190
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-04-28 00:42:36 +0200

    Parse and typecheck on the command line

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,32 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Protolude
+import Protolude hiding (force)
 
+import qualified Text.Parsix as Parsix
+
+import qualified Context
+import qualified Elaboration
+import qualified Evaluation
+import Index
+import Monad
 import qualified Parser
 
 main :: IO ()
 main = do
-  [x] <- getArgs
-  Parser.parseTest Parser.term x
+  [inputString] <- getArgs
+  parseAndTypeCheck inputString
 
+parseAndTypeCheck :: StringConv s Text => s -> IO ()
+parseAndTypeCheck inputString =
+  case Parser.parseText Parser.term (toS inputString) "<command-line>" of
+    Parsix.Success preTerm -> do
+      Elaboration.Inferred term typeValue <- Elaboration.infer Context.empty preTerm
+      print term
+      typeValue' <- force typeValue
+      type_ <- Evaluation.readBack Zero typeValue'
+      print type_
+    Parsix.Failure err -> do
+      putText "Parse error"
+      print $ Parsix.prettyError err
