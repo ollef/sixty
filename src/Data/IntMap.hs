af85425561f333f24cf07d50210402f8de6186f4
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-08-09 08:42:12 +0200

    Add some newlines

--- src/Data/IntMap.hs
+++ src/Data/IntMap.hs
@@ -17,37 +17,49 @@ instance (Hashable key, Hashable value, Coercible Containers.Key key) => Hashabl
   hashWithSalt salt (IntMap m) = hashWithSalt salt (coerce $ Containers.toList m :: [(key, value)])
 
 null :: IntMap key value -> Bool
-null (IntMap m) = Containers.null m
+null (IntMap m) =
+  Containers.null m
 
 singleton :: Coercible key Containers.Key => key -> value -> IntMap key value
-singleton key value = IntMap $ Containers.singleton (coerce key) value
+singleton key value =
+  IntMap $ Containers.singleton (coerce key) value
 
 fromList :: Coercible key Containers.Key => [(key, value)] -> IntMap key value
-fromList xs = IntMap $ Containers.fromList $ coerce xs
+fromList xs =
+  IntMap $ Containers.fromList $ coerce xs
 
 toList :: Coercible key Containers.Key => IntMap key value -> [(key, value)]
-toList (IntMap m) = coerce $ Containers.toList m
+toList (IntMap m) =
+  coerce $ Containers.toList m
 
 insert :: Coercible key Containers.Key => key -> value -> IntMap key value -> IntMap key value
-insert key value = coerce $ Containers.insert (coerce key) value
+insert key value =
+  coerce $ Containers.insert (coerce key) value
 
 adjust :: Coercible key Containers.Key => (value -> value) -> key -> IntMap key value -> IntMap key value
-adjust f key = coerce $ Containers.adjust f (coerce key)
+adjust f key =
+  coerce $ Containers.adjust f (coerce key)
 
 lookup :: Coercible key Containers.Key => key -> IntMap key value -> Maybe value
-lookup key m = Containers.lookup (coerce key) (coerce m)
+lookup key m =
+  Containers.lookup (coerce key) (coerce m)
 
 (!) :: Coercible key Containers.Key => IntMap key value -> key -> value
-m ! key = coerce m Containers.! coerce key
+m ! key =
+  coerce m Containers.! coerce key
 
 lookupDefault :: Coercible key Containers.Key => value -> key -> IntMap key value -> value
-lookupDefault def key m = Containers.findWithDefault def (coerce key) (coerce m)
+lookupDefault def key m =
+  Containers.findWithDefault def (coerce key) (coerce m)
 
 member :: Coercible key Containers.Key => key -> IntMap key value -> Bool
-member key (IntMap m) = Containers.member (coerce key) m
+member key (IntMap m) =
+  Containers.member (coerce key) m
 
 unionWith :: (value -> value -> value) -> IntMap key value -> IntMap key value -> IntMap key value
-unionWith f m1 m2 = IntMap $ Containers.unionWith f (coerce m1) (coerce m2)
+unionWith f m1 m2 =
+  IntMap $ Containers.unionWith f (coerce m1) (coerce m2)
 
 traverseWithKey :: (Coercible key Containers.Key, Applicative t) => (key -> value1 -> t value2) -> IntMap key value1 -> t (IntMap key value2)
-traverseWithKey f (IntMap m) = IntMap <$> Containers.traverseWithKey (coerce f) m
+traverseWithKey f (IntMap m) =
+  IntMap <$> Containers.traverseWithKey (coerce f) m
--- src/Data/IntSet.hs
+++ src/Data/IntSet.hs
@@ -13,25 +13,33 @@ newtype IntSet key = IntSet Containers.IntSet
   deriving (Eq, Ord, Read, Show, Semigroup, Monoid)
 
 instance (Coercible Containers.Key key, Hashable key) => Hashable (IntSet key) where
-  hashWithSalt salt (IntSet s) = hashWithSalt salt (coerce $ Containers.toList s :: [key])
+  hashWithSalt salt (IntSet s) =
+    hashWithSalt salt (coerce $ Containers.toList s :: [key])
 
 null :: IntSet key -> Bool
-null (IntSet s) = Containers.null s
+null (IntSet s) =
+  Containers.null s
 
 singleton :: Coercible key Containers.Key => key -> IntSet key
-singleton key = IntSet $ Containers.singleton $ coerce key
+singleton key =
+  IntSet $ Containers.singleton $ coerce key
 
 fromList :: Coercible key Containers.Key => [key] -> IntSet key
-fromList xs = IntSet $ Containers.fromList $ coerce xs
+fromList xs =
+  IntSet $ Containers.fromList $ coerce xs
 
 toList :: Coercible key Containers.Key => IntSet key -> [key]
-toList (IntSet s) = coerce $ Containers.toList s
+toList (IntSet s) =
+  coerce $ Containers.toList s
 
 insert :: Coercible key Containers.Key => key -> IntSet key -> IntSet key
-insert key = coerce $ Containers.insert $ coerce key
+insert key =
+  coerce $ Containers.insert $ coerce key
 
 delete :: Coercible key Containers.Key => key -> IntSet key -> IntSet key 
-delete key = coerce $ Containers.delete $ coerce key
+delete key =
+  coerce $ Containers.delete $ coerce key
 
 member :: Coercible key Containers.Key => key -> IntSet key -> Bool
-member key (IntSet s) = Containers.member (coerce key) s
+member key (IntSet s) =
+  Containers.member (coerce key) s
