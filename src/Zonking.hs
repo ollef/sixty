4a17d102b301a6e4e3d12eb8148da1494980b22b
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-10-29 22:03:43 +0100

    Add Zonking module

---
+++ src/Zonking.hs
@@ -0,0 +1,139 @@
+module Zonking where
+
+import Protolude
+
+import qualified Domain
+import qualified Environment
+import qualified Evaluation
+import qualified Meta
+import Monad
+import qualified Readback
+import qualified Syntax
+import Syntax.Telescope (Telescope)
+import qualified Syntax.Telescope as Telescope
+
+zonkTerm
+  :: Domain.Environment v
+  -> (Meta.Index -> M (Maybe (Syntax.Term Void)))
+  -> Syntax.Term v
+  -> M (Syntax.Term v)
+zonkTerm env metas term = do
+  eitherValueOrTerm <- zonk env metas term
+  case eitherValueOrTerm of
+    Left value -> do
+      term' <- Readback.readback env value
+      zonkTerm env metas term'
+
+    Right term' ->
+      pure term'
+
+zonkValue
+  :: Domain.Environment v
+  -> (Meta.Index -> M (Maybe (Syntax.Term Void)))
+  -> Syntax.Term v
+  -> M Domain.Value
+zonkValue env metas term = do
+  eitherValueOrTerm <- zonk env metas term
+  case eitherValueOrTerm of
+    Left value ->
+      pure value
+
+    Right term' ->
+      Evaluation.evaluate env term'
+
+zonk
+  :: Domain.Environment v
+  -> (Meta.Index -> M (Maybe (Syntax.Term Void)))
+  -> Syntax.Term v
+  -> M (Either Domain.Value (Syntax.Term v))
+zonk env metas term =
+  case term of
+    Syntax.Var _ ->
+      pure $ Right term
+
+    Syntax.Global _ ->
+      pure $ Right term
+
+    Syntax.Con _ ->
+      pure $ Right term
+
+    Syntax.Meta i -> do
+      maybeTerm <- metas i
+      case maybeTerm of
+        Nothing ->
+          pure $ Right term
+
+        Just term' -> do
+          value <- Evaluation.evaluate (Environment.empty $ Environment.scopeKey env) term'
+          pure $ Left value
+
+    Syntax.Let binding term' type_ scope -> do
+      (env', _) <- Environment.extend env
+      result <- Syntax.Let binding <$>
+        zonkTerm env metas term' <*>
+        zonkTerm env metas type_ <*>
+        zonkTerm env' metas scope
+      pure $ Right result
+
+    Syntax.Pi binding source plicity domainScope -> do
+      (env', _) <- Environment.extend env
+      result <- Syntax.Pi binding <$>
+        zonkTerm env metas source <*>
+        pure plicity <*>
+        zonkTerm env' metas domainScope
+      pure $ Right result
+
+    Syntax.Fun source plicity domain -> do
+      result <- Syntax.Fun <$>
+        zonkTerm env metas source <*>
+        pure plicity <*>
+        zonkTerm env metas domain
+      pure $ Right result
+
+    Syntax.Lam binding type_ plicity bodyScope -> do
+      (env', _) <- Environment.extend env
+      result <- Syntax.Lam binding <$>
+        zonkTerm env metas type_ <*>
+        pure plicity <*>
+        zonkTerm env' metas bodyScope
+      pure $ Right result
+
+    Syntax.App fun plicity arg -> do
+      funResult <- zonk env metas fun
+      case funResult of
+        Left funValue -> do
+          argValue <- zonkValue env metas arg
+          value <- Evaluation.apply funValue plicity argValue
+          pure $ Left value
+
+        Right fun' -> do
+          result <- Syntax.App fun' plicity <$> zonkTerm env metas arg
+          pure $ Right result
+
+    Syntax.Case scrutinee branches defaultBranch -> do
+      result <- Syntax.Case <$>
+        zonkTerm env metas scrutinee <*>
+        forM branches (mapM $ zonkBranch env metas ) <*>
+        forM defaultBranch (zonkTerm env metas)
+      pure $ Right result
+
+    Syntax.Spanned span term' -> do
+      result <- zonk env metas term'
+      pure $ Syntax.Spanned span <$> result
+
+zonkBranch
+  :: Domain.Environment v
+  -> (Meta.Index -> M (Maybe (Syntax.Term Void)))
+  -> Telescope Syntax.Type Syntax.Term v
+  -> M (Telescope Syntax.Type Syntax.Term v)
+zonkBranch env metas tele =
+  case tele of
+    Telescope.Empty branch ->
+      Telescope.Empty <$> zonkTerm env metas branch
+
+    Telescope.Extend binding type_ plicity tele' -> do
+      (env', _) <- Environment.extend env
+      Telescope.Extend binding <$>
+        zonkTerm env metas type_ <*>
+        pure plicity <*>
+        zonkBranch env' metas tele'
