6d2ed0e7df4ea5fd235cff4d76d93aa693f48fce
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-11-25 23:40:21 +0100

    Add LineColumn.fromAbsolute

--- src/LanguageServer/LineColumn.hs
+++ src/LanguageServer/LineColumn.hs
@@ -6,9 +6,11 @@ import Protolude hiding (moduleName)
 import qualified Data.Rope.UTF16 as Rope
 import Rock
 
+import qualified Name
 import qualified Position
 import Query (Query)
 import qualified Query
+import qualified Query.Mapped as Mapped
 import qualified Scope
 import qualified Span
 import Span (LineColumn(LineColumns))
@@ -35,3 +37,29 @@ fromKeyedName keyedName = do
       Span.LineColumns (toLineColumn start) (toLineColumn end)
 
   return $ toLineColumns . Span.absoluteFrom absolutePosition
+
+fromAbsolute :: MonadFetch Query m => Name.Module -> m (Span.Absolute -> Span.LineColumn)
+fromAbsolute moduleName = do
+  maybeFilePath <- fetch $ Query.ModuleFile $ Mapped.Query moduleName
+  case maybeFilePath of
+    Nothing ->
+      pure $ const $ Span.LineColumns (Position.LineColumn 0 0) (Position.LineColumn 0 0)
+
+    Just filePath -> do
+      contents <- fetch $ Query.FileText filePath
+      let
+        -- TODO use the rope that we get from the LSP library instead
+        rope =
+          Rope.fromText contents
+
+        toLineColumn (Position.Absolute i) =
+          let
+            rope' =
+              Rope.take i rope
+          in
+          Position.LineColumn (Rope.rows rope') (Rope.columns rope')
+
+        toLineColumns (Span.Absolute start end) =
+          Span.LineColumns (toLineColumn start) (toLineColumn end)
+
+      return toLineColumns
