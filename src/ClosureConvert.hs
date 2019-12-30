27108ea4a5adff66a297c1d1d369fe944b720eab
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-12-30 01:49:19 +0100

    Add closure conversion

---
+++ src/ClosureConvert.hs
@@ -0,0 +1,167 @@
+{-# language FlexibleContexts #-}
+module ClosureConvert where
+
+import Protolude
+import Rock
+
+import qualified ClosureConverted.Syntax as ClosureConverted
+import qualified LambdaLifted.Syntax as LambdaLifted
+import Query (Query)
+import qualified Query
+import Syntax.Telescope (Telescope)
+import qualified Syntax.Telescope as Telescope
+
+convertDefinition
+  :: MonadFetch Query m
+  => LambdaLifted.Definition
+  -> m ClosureConverted.Definition
+convertDefinition def =
+  case def of
+    LambdaLifted.ConstantDefinition (Telescope.Empty term) ->
+      ClosureConverted.ConstantDefinition <$> convertTerm term []
+
+    LambdaLifted.ConstantDefinition tele ->
+      ClosureConverted.FunctionDefinition <$> convertTelescope tele
+
+    LambdaLifted.DataDefinition (Telescope.Empty (LambdaLifted.ConstructorDefinitions constrDefs)) ->
+      ClosureConverted.DataDefinition . ClosureConverted.ConstructorDefinitions <$>
+        mapM (\type_ -> convertTerm type_ []) constrDefs
+
+    LambdaLifted.DataDefinition tele ->
+      ClosureConverted.ParameterisedDataDefinition <$> convertParameterisedDataDefinition tele
+
+convertParameterisedDataDefinition
+  :: MonadFetch Query m
+  => Telescope LambdaLifted.Type LambdaLifted.ConstructorDefinitions v
+  -> m (Telescope ClosureConverted.Type ClosureConverted.ConstructorDefinitions v)
+convertParameterisedDataDefinition tele =
+  case tele of
+    Telescope.Empty (LambdaLifted.ConstructorDefinitions constrDefs) ->
+      Telescope.Empty . ClosureConverted.ConstructorDefinitions <$> mapM (\type_ -> convertTerm type_ []) constrDefs
+
+    Telescope.Extend binding type_ plicity tele' ->
+      Telescope.Extend binding <$>
+        convertTerm type_ [] <*>
+        pure plicity <*>
+        convertParameterisedDataDefinition tele'
+
+convertTerm
+  :: MonadFetch Query m
+  => LambdaLifted.Term v
+  -> [ClosureConverted.Term v]
+  -> m (ClosureConverted.Term v)
+convertTerm term args =
+  case term of
+    LambdaLifted.Var var ->
+      applyArgs $
+        pure $ ClosureConverted.Var var
+
+    LambdaLifted.Global global -> do
+      maybeDef <- fetch $ Query.LambdaLifted global
+      let
+        nonFunctionCase =
+          applyArgs $ pure $ ClosureConverted.Global global
+
+        functionCase tele =
+          pure $
+            case (Telescope.length tele, length args) of
+              (arity, numArgs)
+                | arity == numArgs ->
+                  ClosureConverted.Apply global args
+
+                | arity < numArgs -> do
+                  let
+                    (preArgs, postArgs) =
+                      splitAt arity args
+
+                  ClosureConverted.ApplyClosure
+                    (ClosureConverted.Apply global preArgs)
+                    postArgs
+
+                | otherwise ->
+                  ClosureConverted.Closure global args
+
+      case maybeDef of
+        Just (LambdaLifted.ConstantDefinition (Telescope.Empty _)) ->
+          nonFunctionCase
+
+        Just (LambdaLifted.DataDefinition (Telescope.Empty _)) ->
+          nonFunctionCase
+
+        Just (LambdaLifted.ConstantDefinition tele) ->
+          functionCase tele
+
+        Just (LambdaLifted.DataDefinition tele) ->
+          functionCase tele
+
+        Nothing ->
+          nonFunctionCase
+
+    LambdaLifted.Con con conArgs ->
+      ClosureConverted.Con con <$>
+        mapM (\arg -> convertTerm arg []) conArgs
+
+    LambdaLifted.Lit lit ->
+      pure $ ClosureConverted.Lit lit
+
+    LambdaLifted.Let binding term' type_ body ->
+      applyArgs $
+        ClosureConverted.Let binding <$>
+          convertTerm term' [] <*>
+          convertTerm type_ [] <*>
+          convertTerm body []
+
+    LambdaLifted.Pi binding domain target ->
+      ClosureConverted.Pi binding <$>
+        convertTerm domain [] <*>
+        convertTerm target []
+
+    LambdaLifted.App fun arg -> do
+      arg' <- convertTerm arg []
+      convertTerm fun $ arg' : args
+
+    LambdaLifted.Case scrutinee branches defaultBranch ->
+      applyArgs $
+        ClosureConverted.Case <$>
+          convertTerm scrutinee [] <*>
+          convertBranches branches <*>
+          mapM (\branch -> convertTerm branch []) defaultBranch
+
+  where
+    applyArgs mresult =
+      case args of
+        [] ->
+          mresult
+
+        _ -> do
+          result <- mresult
+          pure $ ClosureConverted.ApplyClosure result args
+
+convertBranches
+  :: MonadFetch Query m
+  => LambdaLifted.Branches v
+  -> m (ClosureConverted.Branches v)
+convertBranches branches =
+  case branches of
+    LambdaLifted.ConstructorBranches constructorBranches ->
+      ClosureConverted.ConstructorBranches <$>
+        mapM convertTelescope constructorBranches
+
+    LambdaLifted.LiteralBranches literalBranches ->
+      ClosureConverted.LiteralBranches <$>
+        mapM (\branch -> convertTerm branch []) literalBranches
+
+convertTelescope
+  :: MonadFetch Query m
+  => Telescope LambdaLifted.Type LambdaLifted.Term v
+  -> m (Telescope ClosureConverted.Type ClosureConverted.Term v)
+convertTelescope tele =
+  case tele of
+    Telescope.Empty term ->
+      Telescope.Empty <$> convertTerm term []
+
+    Telescope.Extend binding type_ plicity tele' ->
+      Telescope.Extend binding <$>
+        convertTerm type_ [] <*>
+        pure plicity <*>
+        convertTelescope tele'
---
+++ src/ClosureConverted/Syntax.hs
@@ -0,0 +1,68 @@
+{-# language DeriveAnyClass #-}
+{-# language DeriveGeneric #-}
+module ClosureConverted.Syntax where
+
+import Protolude hiding (Type, IntMap)
+
+import Data.HashMap.Lazy (HashMap)
+
+import Binding (Binding)
+import Index
+import Literal (Literal)
+import qualified Name
+import Syntax.Telescope (Telescope)
+
+{-
+General idea:
+
+xs |- t : T
+-------------------------
+|- \xs. t : Fun xs T
+
+f : Fun xs T
+ts : xs[ts/xs]
+-----------------
+f ts : T[ts/xs]
+
+f : Fun (xs ++ ys) T
+ts : xs[ts / xs]
+--------------------------------------
+Closure f ts : ys[ts/xs] -> T[ts / xs]
+
+cl : (xs ++ ys) -> T
+ts : xs[ts/xs]
+------------------------------------------
+ApplyClosure cl ts : ys[ts/xs] -> T[ts/xs]
+-}
+
+data Term v
+  = Var !(Index v)
+  | Global !Name.Lifted
+  | Con !Name.QualifiedConstructor [Term v]
+  | Lit !Literal
+  | Let !Binding !(Term v) !(Type v) !(Scope Term v)
+  | Function !(Telescope Type Term Void) -- ^ The type of a top-level function definition
+  | Apply !Name.Lifted [Term v] -- ^ Application of a top-level function
+  | Pi !Binding !(Type v) !(Scope Type v)
+  | Closure !Name.Lifted [Term v]
+  | ApplyClosure !(Term v) [Term v]
+  | Case !(Term v) (Branches v) !(Maybe (Term v))
+  deriving (Eq, Show, Generic, Hashable)
+
+type Type = Term
+
+data Branches v
+  = ConstructorBranches (HashMap Name.QualifiedConstructor (Telescope Type Term v))
+  | LiteralBranches (HashMap Literal (Term v))
+  deriving (Eq, Show, Generic, Hashable)
+
+data Definition
+  = ConstantDefinition !(Term Void)
+  | FunctionDefinition !(Telescope Type Term Void)
+  | DataDefinition (ConstructorDefinitions Void)
+  | ParameterisedDataDefinition !(Telescope Type ConstructorDefinitions Void)
+  deriving (Show, Generic, Hashable)
+
+newtype ConstructorDefinitions v =
+  ConstructorDefinitions (HashMap Name.Constructor (Type v))
+  deriving (Show, Generic, Hashable)
