9ae364a7b0a22848c3321a2e74e151f7b96c06e9
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-04-07 19:29:47 +0200

    Use numCapabilities up to 8
    4 threads seems to work better on my laptop with 4 cores, whereas 8
    threads works the best on my 8 core/16 thread stationary computer.
    
    This needs to be revisited on other CPUs but this will give the sweet
    spot on both my machines.

--- src/Driver.hs
+++ src/Driver.hs
@@ -317,7 +317,7 @@ pooledForConcurrentlyIO_ as f = do
               _ <- f a
               go
             )
-  replicateConcurrently_ ((processCount + 1) `div` 2) go
+  replicateConcurrently_ (max 8 processCount) go
 
 pooledForConcurrentlyIO
   :: Traversable t
@@ -344,7 +344,7 @@ pooledForConcurrentlyIO as f = do
               atomicWriteIORef ref result
               go
             )
-  replicateConcurrently_ ((processCount + 1) `div` 2) go
+  replicateConcurrently_ (max 8 processCount) go
   forM jobs $ \(_, ref) ->
     readIORef ref
 
