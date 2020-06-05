c291c87c8712657d4e478bf51a98b1c26ff4056c
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-06-05 15:23:42 +0200

    Add ClosureConverted.Context.emptyFrom

--- src/ClosureConverted/Context.hs
+++ src/ClosureConverted/Context.hs
@@ -2,7 +2,7 @@
 {-# language DuplicateRecordFields #-}
 module ClosureConverted.Context where
 
-import Protolude hiding (IntMap)
+import Protolude hiding (empty, IntMap)
 
 import qualified ClosureConverted.Domain as Domain
 import Data.IntMap (IntMap)
@@ -32,6 +32,10 @@ empty scopeKey_ =
     , values = mempty
     , types = mempty
     }
+
+emptyFrom :: Context v -> Context Void
+emptyFrom context =
+  empty $ scopeKey context
 
 lookupVarType :: Var -> Context v -> Domain.Type
 lookupVarType var context =
