3c193e7ab7c4af62366ef80d522186bd15df2734
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-06-05 15:23:49 +0200

    Add ClosureConverted.Context.extendUnindexed

--- src/ClosureConverted/Context.hs
+++ src/ClosureConverted/Context.hs
@@ -66,3 +66,16 @@ extend context type_ = do
       }
     , var
     )
+
+extendUnindexed
+  :: Context v
+  -> Domain.Type
+  -> M (Context v, Var)
+extendUnindexed context type_ = do
+  var <- freshVar
+  pure
+    ( context
+      { types = IntMap.insert var type_ (types context)
+      }
+    , var
+    )
