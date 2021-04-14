e425938e3f1ab26c6107b2d8c73841941b441fc3
Olle Fredriksson <fredriksson.olle@gmail.com>
2021-04-14 19:12:40 +0200

    Remove unused stack init code

--- rts/init.c
+++ rts/init.c
@@ -3,15 +3,6 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
-char* init_stack() {
-  size_t size = 16 * 1024 * 1024; // 16 MB
-  char* address = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-  if (address == MAP_FAILED) {
-    exit(EXIT_FAILURE);
-  }
-  return address + size;
-}
-
 char* init_global_area() {
   size_t size = 16 * 1024 * 1024;
   char* address = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
--- rts/main.ll
+++ rts/main.ll
@@ -1,9 +1,7 @@
-declare i8* @init_stack()
 declare i8* @init_global_area()
 declare fastcc void @.$module_init(i64*)
 
 define i32 @main() {
-  %stack = call i8* @init_stack()
   %global_area = call i8* @init_global_area()
   %global_area_i64ptr = bitcast i8* %global_area to i64*
 
